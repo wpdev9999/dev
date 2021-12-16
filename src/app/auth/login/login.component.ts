e8a453ae9be8870041e9fdfc0e56b8f5d1aa1865
Dipak <wpdev9999@gmail.com>
2021-12-16 20:24:25 +0530

    form merge

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -11,23 +11,16 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   
   login : FormGroup;
-  register : FormGroup;
+  
   get f(): any { return this.login.controls; }
-  get r(): any { return this.register.controls; }
    
     
   constructor(private fb:FormBuilder, private _snackBar: MatSnackBar, private router: Router) {
     this.login = this.fb.group({
       userName: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    });
-
-    this.register = this.fb.group({
-      userName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
-
    }
 
   ngOnInit(): void {
@@ -43,15 +36,8 @@ export class LoginComponent implements OnInit {
   }
 
   onRegister(){
-    if(this.register.valid){
-      this._snackBar.open('Registration successfully.','Close');
-      this.router.navigate(['/dashboard']);
-      localStorage.setItem('token','svsddbdsbdbdbb');
-      localStorage.setItem('userType','admin');
-    }
+    console.log('Register');
   }
-
-
-  
+   
 
 }
