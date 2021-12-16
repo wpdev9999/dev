93aa711a11c5924b03bff40fa2cbe6d08e78dc34
Dipak Patel <wpdev9999@gmail.com>
2021-12-17 01:48:09 +0530

    Register form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -9,17 +9,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  
+    
   login : FormGroup;
-  
+  register : FormGroup;
+    
   get f(): any { return this.login.controls; }
+  get r(): any { return this.register.controls; }
    
     
   constructor(private fb:FormBuilder, private _snackBar: MatSnackBar, private router: Router) {
     this.login = this.fb.group({
       userName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
       password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+
+    this.register = this.fb.group({
+      userName: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      password: [null, [Validators.required, Validators.minLength(6)]]
     });
    }
 
@@ -28,15 +35,17 @@ export class LoginComponent implements OnInit {
 
   onSubmit(){
     if(this.login.valid){
-      this._snackBar.open('Done','Close');
+      this._snackBar.open('Login successfully','Close');
       this.router.navigate(['/dashboard']);
       localStorage.setItem('token','svsddbdsbdbdbb');
       localStorage.setItem('userType','admin');
-    }
+    }    
   }
 
   onRegister(){
     console.log('Register');
+    this.register.reset();
+    //this.register.setErrors(null);
   }
    
 
